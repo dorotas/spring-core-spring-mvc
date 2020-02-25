576c269a1b21b08a458249f73e5be83772d64c4b
dsuchocka <dsuchocka@gorillagroup.com>
2020-02-25 12:56:03 +0100

    after section 2

---
+++ src/main/java/guru/springframework/services/reposervices/ProductServiceRepoImpl.java
@@ -0,0 +1,41 @@
+package guru.springframework.services.reposervices;
+
+import guru.springframework.domain.Product;
+import guru.springframework.repositories.ProductRepository;
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile("springdatajpa")
+public class ProductServiceRepoImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<?> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product domainObject) {
+        return productRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.delete(id);
+    }
+}
---
+++ src/main/java/guru/springframework/services/reposervices/UserServiceRepoImpl.java
@@ -0,0 +1,49 @@
+package guru.springframework.services.reposervices;
+
+import guru.springframework.domain.User;
+import guru.springframework.repositories.UserRepository;
+import guru.springframework.services.UserService;
+import guru.springframework.services.security.EncryptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile("springdatajpa")
+public class UserServiceRepoImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EncryptionService encryptionService;
+
+    @Override
+    public List<?> listAll() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    @Override
+    public User getById(Integer id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public User saveOrUpdate(User domainObject) {
+
+        if(domainObject.getPassword() != null){
+            domainObject.setEncryptedPassword(encryptionService.encryptString(domainObject.getPassword()));
+        }
+        return userRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        userRepository.delete(id);
+    }
+}
