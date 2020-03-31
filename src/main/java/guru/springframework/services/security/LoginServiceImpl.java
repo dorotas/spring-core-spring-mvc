ace1f04ed0523e830a1d80cf902db7389799d6fa
dsuchocka <dsuchocka@gorillagroup.com>
2020-03-31 15:31:59 +0200

    scheduler

---
+++ src/main/java/guru/springframework/services/security/LoginServiceImpl.java
@@ -0,0 +1,32 @@
+package guru.springframework.services.security;
+
+import guru.springframework.domain.User;
+import guru.springframework.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    @Scheduled(fixedRate = 60000)
+    public void resetFailedLogins() {
+        System.out.println("Checking for locked accounts");
+
+        List<User> userList = (List<User>) userService.listAll();
+        userList.forEach(user -> {
+            if (!user.getEnabled() && user.getFailedLoginAttempts() > 0) {
+                System.out.println("Resetting failed attempts for user: " + user.getUsername());
+                user.setFailedLoginAttempts(0);
+                user.setEnabled(true);
+                userService.saveOrUpdate(user);
+            }
+        });
+    }
+}
